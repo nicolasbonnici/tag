5e50c999761d357e00982cbd3270650f6041d8b3
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-05-23 00:20:47 +0200

    "Auto generated commit message for tag bundle by sociableCore framework console app, see diff for more infos"

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -20,4 +20,42 @@ class HomeController extends \Library\Core\Auth
         $this->oView->render($this->aView, 'home/index.tpl');
     }
 
+
+    public function createAction()
+    {
+        $this->oView->render($this->aView, 'tag/create.tpl');
+    }
+
+    public function readAction()
+    {
+        if (isset($this->aParams['idtag']) && intval($this->aParams['idtag']) > 0) {
+            $oTagModel = new \bundles\backend\Models\Tag(intval($this->aParams['idtag']), $this->oUser);
+            $oTag = $oTagModel->read();
+            if (! is_null($oTag) && $oTag->isLoaded()) {
+                $this->aView['oTag'] = $oTag;
+            }
+        }
+        $this->oView->render($this->aView, 'tag/read.tpl');
+    }
+
+    public function updateAction()
+    {
+        if (isset($this->aParams['idtag']) && intval($this->aParams['idtag']) > 0) {
+            $oTagModel = new \bundles\backend\Models\Tag(intval($this->aParams['idtag']), $this->oUser);
+            $oTag = $oTagModel->getEntity();
+            if (! is_null($oTag) && $oTag->isLoaded()) {
+                $this->aView['oTag'] = $oTag;
+            }
+        }
+        $this->oView->render($this->aView, 'tag/update.tpl');
+    }
+
+    public function deleteAction()
+    {
+        if (isset($this->aParams['pk']) && intval($this->aParams['pk']) > 0) {
+            $this->aView['pk'] = $this->aParams['pk'];
+        }
+        $this->oView->render($this->aView, 'tag/delete.tpl');
+    }
+
 }
